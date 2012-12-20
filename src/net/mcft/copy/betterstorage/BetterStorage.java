3dfb0e10dde97cad3b2b61d28b532806ca6e75bd
Sven Braune <copyboy@hotmail.de>
2012-12-20 12:07:50 +0100

    Change version number to 0.3.2

--- src/net/mcft/copy/betterstorage/BetterStorage.java
+++ src/net/mcft/copy/betterstorage/BetterStorage.java
@@ -39,7 +39,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 
-@Mod(modid="BetterStorage", name="BetterStorage", version="0.3.1")
+@Mod(modid="BetterStorage", name="BetterStorage", version="0.3.2")
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
 public class BetterStorage {
 	
