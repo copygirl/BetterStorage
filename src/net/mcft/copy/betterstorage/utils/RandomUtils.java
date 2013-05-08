60c01b425ac561dbc3da9f9904abbba2ebd0d5fb
copyboy <copyboy@hotmail.de>
2013-05-08 03:30:46 +0200

    Add RandomUtils.getDouble

--- src/net/mcft/copy/betterstorage/utils/RandomUtils.java
+++ src/net/mcft/copy/betterstorage/utils/RandomUtils.java
@@ -7,14 +7,15 @@ public class RandomUtils {
 	private static final Random random = new Random();
 	
 	public static int getInt(int max) { return random.nextInt(max); }
-	
 	public static int getInt(int min, int max) { return min + getInt(max - min); }
 	
 	public static float getFloat() { return random.nextFloat(); }
-	
 	public static float getFloat(float max) { return getFloat() * max; }
-	
 	public static float getFloat(float min, float max) { return min + getFloat(max - min); }
+	
+	public static double getDouble() { return random.nextDouble(); }
+	public static double getDouble(double max) { return getDouble() * max; }
+	public static double getDouble(double min, double max) { return min + getDouble(max - min); }
 	
 	public static double getGaussian() { return random.nextGaussian(); }
 	
