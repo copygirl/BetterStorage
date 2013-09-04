05ab820a4bc3c90fd35d80135fbc955bb218328d
copyboy <copyboy@hotmail.de>
2013-09-04 15:41:46 +0200

    Wait I can do this better!

--- src/net/mcft/copy/betterstorage/utils/EntityUtils.java
+++ src/net/mcft/copy/betterstorage/utils/EntityUtils.java
@@ -1,28 +1,43 @@
 package net.mcft.copy.betterstorage.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.minecraft.entity.Entity;
 import net.minecraftforge.common.IExtendedEntityProperties;
 
 public final class EntityUtils {
 	
+	private static Map<Class, String> propertiesLookup = new HashMap<Class, String>();
+	
 	private EntityUtils() {  }
 	
-	public static <T extends IExtendedEntityProperties> T getProperties(Entity entity, Class<T> propertiesClass, String identifier) {
-		IExtendedEntityProperties properties = entity.getExtendedProperties(identifier);
+	public static String getIdentifier(Class<? extends IExtendedEntityProperties> propertiesClass) {
+		String identifier = propertiesLookup.get(propertiesClass);
+		if (identifier == null) {
+			try { identifier = (String)propertiesClass.getField("identifier").get(null); }
+			catch (Exception e) { throw new Error(e); }
+			propertiesLookup.put(propertiesClass, identifier);
+		}
+		return identifier;
+	}
+	
+	public static <T extends IExtendedEntityProperties> T getProperties(Entity entity, Class<T> propertiesClass) {
+		IExtendedEntityProperties properties = entity.getExtendedProperties(getIdentifier(propertiesClass));
 		return (propertiesClass.isInstance(properties) ? (T)properties : null);
 	}
 	
-	public static <T extends IExtendedEntityProperties> T createProperties(Entity entity, Class<T> propertiesClass, String identifier) {
+	public static <T extends IExtendedEntityProperties> T createProperties(Entity entity, Class<T> propertiesClass) {
 		try {
 			T properties = propertiesClass.getConstructor().newInstance();
-			entity.registerExtendedProperties(identifier, properties);
+			entity.registerExtendedProperties(getIdentifier(propertiesClass), properties);
 			return properties;
-		} catch (Exception e) { throw new RuntimeException(e); }
+		} catch (Exception e) { throw new Error(e); }
 	}
 	
-	public static <T extends IExtendedEntityProperties> T getOrCreateProperties(Entity entity, Class<T> propertiesClass, String identifier) {
-		T properties = getProperties(entity, propertiesClass, identifier);
-		return ((properties != null) ? properties : createProperties(entity, propertiesClass, identifier));
+	public static <T extends IExtendedEntityProperties> T getOrCreateProperties(Entity entity, Class<T> propertiesClass) {
+		T properties = getProperties(entity, propertiesClass);
+		return ((properties != null) ? properties : createProperties(entity, propertiesClass));
 	}
 	
 }
