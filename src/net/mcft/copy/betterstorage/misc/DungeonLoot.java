00f1fca5720f725f75e43a79508cd0b06e4dacc7
copyboy <copyboy@hotmail.de>
2013-10-28 15:24:00 +0100

    Close #113, dungeon loot adjustments

--- src/net/mcft/copy/betterstorage/misc/DungeonLoot.java
+++ src/net/mcft/copy/betterstorage/misc/DungeonLoot.java
@@ -1,11 +1,14 @@
 package net.mcft.copy.betterstorage.misc;
 
+import java.util.Random;
+
 import net.mcft.copy.betterstorage.content.Blocks;
 import net.mcft.copy.betterstorage.content.Items;
 import net.mcft.copy.betterstorage.item.ItemDrinkingHelmet;
 import net.mcft.copy.betterstorage.utils.NbtUtils;
 import net.mcft.copy.betterstorage.utils.StackUtils;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagList;
@@ -20,11 +23,11 @@ public final class DungeonLoot {
 		
 		if (Items.drinkingHelmet != null) {
 			ItemStack drinkingHelmet = new ItemStack(Items.drinkingHelmet);
-			addMultiple(drinkingHelmet, ChestGenHooks.DUNGEON_CHEST, 2,
-			                            ChestGenHooks.PYRAMID_DESERT_CHEST, 3,
-			                            ChestGenHooks.PYRAMID_JUNGLE_CHEST, 3,
-			                            ChestGenHooks.STRONGHOLD_CORRIDOR, 3,
-			                            ChestGenHooks.STRONGHOLD_CROSSING, 2);
+			addMultiple(drinkingHelmet, ChestGenHooks.DUNGEON_CHEST, 1.0,
+			                            ChestGenHooks.PYRAMID_DESERT_CHEST, 2.0,
+			                            ChestGenHooks.PYRAMID_JUNGLE_CHEST, 2.0,
+			                            ChestGenHooks.STRONGHOLD_CORRIDOR, 1.0,
+			                            ChestGenHooks.STRONGHOLD_CROSSING, 1.0);
 			
 			ItemStack drinkingHelmetSpecial = new ItemStack(Items.drinkingHelmet);
 			drinkingHelmetSpecial.setItemName("Nishmet");
@@ -36,7 +39,7 @@ public final class DungeonLoot {
 			drinkingHelmetSpecial.addEnchantment(Enchantment.unbreaking, 2);
 			NBTTagList drinkingHelmetLore = NbtUtils.createList(null, "\"i fail at names for items\" -Nishtown");
 			StackUtils.set(drinkingHelmetSpecial, drinkingHelmetLore, "display", "Lore");
-			addMultiple(drinkingHelmetSpecial, ChestGenHooks.PYRAMID_DESERT_CHEST, 1);
+			addMultiple(drinkingHelmetSpecial, ChestGenHooks.PYRAMID_DESERT_CHEST, 0.2);
 		}
 		
 		if (Blocks.backpack != null) {
@@ -44,24 +47,39 @@ public final class DungeonLoot {
 			backpackSpecial1.setItemName("Everlasting Backpack");
 			backpackSpecial1.addEnchantment(Enchantment.unbreaking, 4);
 			StackUtils.set(backpackSpecial1, 0x006622, "display", "color");
-			addMultiple(backpackSpecial1, ChestGenHooks.PYRAMID_JUNGLE_CHEST, 1);
+			addMultiple(backpackSpecial1, ChestGenHooks.PYRAMID_JUNGLE_CHEST, 0.5);
 			
 			ItemStack backpackSpecial2 = new ItemStack(Blocks.backpack);
 			backpackSpecial2.setItemName("Shielding Backpack");
 			backpackSpecial2.addEnchantment(Enchantment.protection, 5);
 			StackUtils.set(backpackSpecial2, 0x0000BB, "display", "color");
-			addMultiple(backpackSpecial2, ChestGenHooks.PYRAMID_DESERT_CHEST, 1);
+			addMultiple(backpackSpecial2, ChestGenHooks.PYRAMID_DESERT_CHEST, 0.4);
 		}
 		
 	}
 	
 	private static void addMultiple(ItemStack stack, Object... args) {
 		for (int i = 0; i < args.length; i += 2)
-			ChestGenHooks.addItem((String)args[i], makeItem(stack, (Integer)args[i + 1]));
+			ChestGenHooks.addItem((String)args[i], makeItem(stack, (Double)args[i + 1]));
 	}
 	
-	private static WeightedRandomChestContent makeItem(ItemStack stack, int weight) {
-		return new WeightedRandomChestContent(stack, 1, 1, weight);
+	private static WeightedRandomChestContent makeItem(ItemStack stack, double chance) {
+		if (chance < 1.0) return new RareChestContent(stack, chance);
+		return new WeightedRandomChestContent(stack, 1, 1, (int)chance);
+	}
+	
+	
+	private static class RareChestContent extends WeightedRandomChestContent {
+		private final double chance;
+		public RareChestContent(ItemStack stack, double chance) {
+			super(stack, 1, 1, 1);
+			this.chance = chance;
+		}
+		@Override
+		protected ItemStack[] generateChestContent(Random random, IInventory newInventory) {
+			if (random.nextDouble() >= chance) return new ItemStack[0];
+			else return super.generateChestContent(random, newInventory);
+		}
 	}
 	
 }
