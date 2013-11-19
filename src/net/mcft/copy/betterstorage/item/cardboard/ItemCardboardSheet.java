88fa469b059c95d26ee304c880038c8ac712ad01
copyboy <copyboy@hotmail.de>
2013-11-19 16:48:20 +0100

    Increase durability of cardboard items

--- src/net/mcft/copy/betterstorage/item/cardboard/ItemCardboardSheet.java
+++ src/net/mcft/copy/betterstorage/item/cardboard/ItemCardboardSheet.java
@@ -8,9 +8,9 @@ import net.minecraftforge.common.EnumHelper;
 public class ItemCardboardSheet extends ItemBetterStorage {
 
 	public static final EnumToolMaterial toolMaterial =
-			EnumHelper.addToolMaterial("cardboard", 0, 48, 2.5F, 0.0F, 18);
+			EnumHelper.addToolMaterial("cardboard", 0, 64, 2.5F, 0.0F, 18);
 	public static final EnumArmorMaterial armorMaterial =
-			EnumHelper.addArmorMaterial("cardboard", 3, new int[]{ 1, 2, 2, 1 }, 20);
+			EnumHelper.addArmorMaterial("cardboard", 5, new int[]{ 1, 2, 2, 1 }, 20);
 	
 	public ItemCardboardSheet(int id) {
 		super(id);
