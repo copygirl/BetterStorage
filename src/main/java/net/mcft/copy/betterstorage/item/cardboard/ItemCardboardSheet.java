d909643f8742bac4912b0186f0573bd9e8ddcfe5
copygirl <halfnerd.copygirl@gmail.com>
2014-02-20 13:06:46 +0100

    Lower cardboard item efficiency and damage

--- src/main/java/net/mcft/copy/betterstorage/item/cardboard/ItemCardboardSheet.java
+++ src/main/java/net/mcft/copy/betterstorage/item/cardboard/ItemCardboardSheet.java
@@ -15,7 +15,7 @@ import net.minecraftforge.common.EnumHelper;
 public class ItemCardboardSheet extends ItemBetterStorage {
 
 	public static final EnumToolMaterial toolMaterial =
-			EnumHelper.addToolMaterial("cardboard", 0, 64, 2.5F, 0.0F, 0);
+			EnumHelper.addToolMaterial("cardboard", 0, 64, 2.0F, -0.5F, 0);
 	public static final EnumArmorMaterial armorMaterial =
 			EnumHelper.addArmorMaterial("cardboard", 5, new int[]{ 1, 2, 2, 1 }, 0);
 	
