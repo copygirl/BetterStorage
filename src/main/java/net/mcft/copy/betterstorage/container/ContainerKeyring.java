0915b2bcb89160a62fadd9b516de7dc227c483b2
copygirl <halfnerd.copygirl@gmail.com>
2014-05-21 15:26:05 +0200

    Pretty up ContainerKeyring, less workaround

--- src/main/java/net/mcft/copy/betterstorage/container/ContainerKeyring.java
+++ src/main/java/net/mcft/copy/betterstorage/container/ContainerKeyring.java
@@ -2,43 +2,32 @@ package net.mcft.copy.betterstorage.container;
 
 import net.mcft.copy.betterstorage.inventory.InventoryKeyring;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
 
 public class ContainerKeyring extends ContainerBetterStorage {
 	
-	private static int protectedIndex = -1;
+	private final int protectedIndex;
 	
-	public ContainerKeyring(EntityPlayer player, String title) {
+	public ContainerKeyring(EntityPlayer player, String title, int protectedIndex) {
 		super(player, new InventoryKeyring(player, title), 9, 1);
-	}
-	
-	public static void setProtectedIndex(int index) {
-		protectedIndex = index;
-	}
-	private int getProtectedIndex() {
-		int index = protectedIndex;
-		protectedIndex = -1;
-		return ((index >= 0) ? index : player.inventory.currentItem);
+		this.protectedIndex = protectedIndex;
+		// We need to do this here instead of the super constructor,
+		// because otherwise we wouldn't be able to set protectedIndex.
+		super.setupInventoryContainer();
+		super.setupInventoryPlayer();
 	}
 	
 	@Override
-	protected void setupInventoryPlayer() {
-		for (int y = 0; y < 3; y++)
-			for (int x = 0; x < 9; x++)
-				addSlot(player.inventory, 9 + x + y * 9,
-				        8 + x * 18 + (getColumns() - 9) * 9,
-				        getRows() * 18 + y * 18 + 32);
-		setHotbarStart();
-		for (int x = 0; x < 9; x++)
-			addSlot(player.inventory, x,
-			        8 + x * 18 + (getColumns() - 9) * 9,
-			        getRows() * 18 + 90);
-	}
+	protected void setupInventoryContainer() {  }
+	@Override
+	protected void setupInventoryPlayer() {  }
 	
-	private void addSlot(IInventory inventory, int slotId, int x, int y) {
-		SlotBetterStorage slot = new SlotBetterStorage(this, inventory, slotId, x, y);
-		if (slotId == getProtectedIndex()) slot.setProtected();
-		addSlotToContainer(slot);
+	@Override
+	protected Slot addSlotToContainer(Slot slot) {
+		if ((slot.inventory == player.inventory) &&
+		    (slot.getSlotIndex() == protectedIndex))
+			((SlotBetterStorage)slot).setProtected();
+		return super.addSlotToContainer(slot);
 	}
 	
 }
