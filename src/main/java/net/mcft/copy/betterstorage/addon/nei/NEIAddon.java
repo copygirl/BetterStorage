a92c62f3107e9dc59e85a086a270cd96d9c4d5da
Victorious3 <Minefoto0@gmail.com>
2014-08-07 10:19:34 +0200

    Made NEI's shift-clicking ingredients possible for Crafting Stations

--- src/main/java/net/mcft/copy/betterstorage/addon/nei/NEIAddon.java
+++ src/main/java/net/mcft/copy/betterstorage/addon/nei/NEIAddon.java
@@ -5,6 +5,8 @@ import net.mcft.copy.betterstorage.addon.Addon;
 import net.mcft.copy.betterstorage.client.gui.GuiCraftingStation;
 import net.mcft.copy.betterstorage.content.BetterStorageTiles;
 import net.mcft.copy.betterstorage.misc.Constants;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import codechicken.nei.api.API;
 import codechicken.nei.recipe.DefaultOverlayHandler;
@@ -26,8 +28,24 @@ public class NEIAddon extends Addon {
 		
 		API.registerGuiOverlay(GuiCraftingStation.class, Constants.modId + ".craftingStation");
 		API.registerGuiOverlay(GuiCraftingStation.class, "crafting", -8, 11);
-		API.registerGuiOverlayHandler(GuiCraftingStation.class, new DefaultOverlayHandler(), Constants.modId + ".craftingStation");
-		API.registerGuiOverlayHandler(GuiCraftingStation.class, new DefaultOverlayHandler(-8, 11), "crafting");
+		
+		API.registerGuiOverlayHandler(GuiCraftingStation.class, new DefaultOverlayHandler() {
+			
+			@Override
+			public boolean canMoveFrom(Slot slot, GuiContainer gui) {
+				return slot.slotNumber >= 18;
+			}
+			
+		}, Constants.modId + ".craftingStation");
+		
+		API.registerGuiOverlayHandler(GuiCraftingStation.class, new DefaultOverlayHandler(-8, 11) {
+			
+			@Override
+			public boolean canMoveFrom(Slot slot, GuiContainer gui) {
+				return slot.slotNumber >= 18;
+			}
+			
+		}, "crafting");
 		
 		API.hideItem(new ItemStack(BetterStorageTiles.lockableDoor));
 	}
