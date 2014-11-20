a79ffcd91aa7720703bf88895aa0665257850932
copygirl <halfnerd.copygirl@gmail.com>
2014-11-20 08:43:08 +0100

    Update to new Armourer's Workshop API

--- src/main/java/net/mcft/copy/betterstorage/addon/armourersworkshop/AWDataManager.java
+++ src/main/java/net/mcft/copy/betterstorage/addon/armourersworkshop/AWDataManager.java
@@ -1,6 +1,7 @@
 package net.mcft.copy.betterstorage.addon.armourersworkshop;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.tileentity.TileEntity;
 import riskyken.armourersWorkshop.api.client.render.IEquipmentRenderHandler;
 import riskyken.armourersWorkshop.api.client.render.IEquipmentRenderManager;
 import riskyken.armourersWorkshop.api.common.equipment.EnumEquipmentPart;
@@ -8,25 +9,27 @@ import riskyken.armourersWorkshop.api.common.equipment.EnumEquipmentType;
 import riskyken.armourersWorkshop.api.common.equipment.IEquipmentDataHandler;
 import riskyken.armourersWorkshop.api.common.equipment.IEquipmentDataManager;
 
+import com.mojang.authlib.GameProfile;
+
 public class AWDataManager implements IEquipmentDataManager, IEquipmentRenderManager {
 	
 	@Override
 	public void onLoad(IEquipmentDataHandler dataHandler) {
 		AWAddon.dataHandler = dataHandler;
 	}
-
+	
 	@Override
 	public void onLoad(IEquipmentRenderHandler renderHandler) {
 		AWAddon.renderHandler = renderHandler;
 	}
-
-	@Override
-	public void onRenderEquipment(Entity entity, EnumEquipmentType armourType) {
-		
-	}
 	
 	@Override
-	public void onRenderEquipmentPart(Entity entity, EnumEquipmentPart armourPart) {
-		
-	}
+	public void onRenderEquipment(Entity entity, EnumEquipmentType armourType) {  }
+	
+	@Override
+	public void onRenderEquipmentPart(Entity entity, EnumEquipmentPart armourPart) {  }
+	
+	@Override
+	public void onRenderMannequin(TileEntity TileEntity, GameProfile gameProfile) {  }
+	
 }
